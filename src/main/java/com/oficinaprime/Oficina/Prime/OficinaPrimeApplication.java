92142867a17d7b4ea4bde79eff1599bcc62c923d
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-08-14 19:54:08 -0300

    First Commit

---
+++ src/main/java/com/oficinaprime/Oficina/Prime/OficinaPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.oficinaprime.Oficina.Prime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaPrimeApplication.class, args);
+	}
+
+}
